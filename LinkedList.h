17f4bc87f1fb906a1e00f6654b9c7605ccf34eaa
chominchae <129182620+chominchae@users.noreply.github.com>
2024-05-18 22:15:33 +0900

    Create LinkedList.h

---
+++ LinkedList.h
@@ -0,0 +1,55 @@
+#include<stdlib.h>
+#define MAX_LIST 100
+#include<stdio.h>
+
+//typedef enum{false,true} bool;
+typedef int Data;
+
+typedef struct {
+	//실제로는 이거 이용
+	Data items[MAX_LIST];
+	int len;
+} ArrayList;
+
+void InitList(ArrayList* plist) {
+	plist->len = 0;
+}
+
+bool IsEmpty(ArrayList* plist){
+	return plist->len == 0;
+}
+
+bool IsFull(ArrayList* plist){
+	return plist->len == MAX_LIST;
+}
+
+void InsertMiddle(ArrayList * plist,int pos,Data item){
+	if (IsFull(plist) || pos<0 || pos>plist->len)
+		exit(1);
+	else 
+		for(int i = plist->len-1; i >=pos; i--) {
+			plist->items[i + 1] = plist->items[i];
+	}
+	plist->items[pos] = item;
+	plist->len++;
+}
+
+void RemoveMiddle(ArrayList* plist, int pos){
+	if (IsEmpty(plist) || pos<0 || pos>plist->len)
+		exit(1);
+	else 
+		for (int i = pos + 1; i < plist->len; i++)
+			plist->items[i] = plist->items[i + 1];
+		
+	plist->len--;
+}
+
+Data ReadItem(ArrayList* plist, int pos) {
+	return plist->items[pos];
+}
+
+void PrintList(ArrayList* plist){
+	for (int i = 0; i < plist->len; i++)
+		printf("items[%d]=%d",i,plist->items[i]);
+}
+
